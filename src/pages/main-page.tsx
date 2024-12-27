c1aeda8aea6c0702ed2fa53ca2c20091c5e2e17d
David <larin.d1805@gmail.com>
2024-12-27 14:53:44 +0300

    feat: main page, post page

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,3 +1,10 @@
+import { postApi, Post } from '@/modules/post';
+import { Loader } from '@/shared/ui/loader';
+
 export function MainPage() {
-  return <div>Main Page</div>;
+  const { data, isLoading, error } = postApi.getPosts();
+
+  if (!!error) return <div>{error}</div>;
+
+  return <>{isLoading ? <Loader /> : <Post.List posts={data} />}</>;
 }
